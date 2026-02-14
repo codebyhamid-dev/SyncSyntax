af9da8f708123fe9b16523278e1ba7f1eb3361d3
Engr. Hamid Idrees <hamididrees457@gmail.com>
2026-02-14 18:42:25 +0500

    implement post model

---
+++ SyncSyntax/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SyncSyntax.Data;
+using SyncSyntax.Models.ViewModels;
+
+namespace SyncSyntax.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+            new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.CategoryName,
+
+            }).ToList();
+            return View(postViewModel);
+        }
+
+    
+    }
+}
---
+++ SyncSyntax/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SyncSyntax.Models.ViewModels
+{
+    public class PostViewModel
+    {
+        public Post Post { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IFormFile FeatureImage { get; set; }
+    }
+}
